bac32d59a24a8cbce06050aec6cbaad15bd675c8
h3lloworld <astroneer976@gmail.com>
2024-07-13 16:54:06 +1000

    Add post method for search function

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,10 @@ import fs from "fs";
 
 // Start up the Rest API
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const port = 8080;
 app.use(cors());
 app.listen(port, () => console.log(`App is running on port ${port}`));// 4
@@ -29,6 +33,33 @@ app.get("/names", async (req: Request, res: Response) => {
     }
 })
 
+// Post: search name by given input
+app.post("/search", (req: Request, res: Response) => {
+    // read the search key word from request body e.g. {"input": "Lee"}
+    const { input } = req.body
+    // validate request
+    if (!input){
+        // 400 Bad Request
+        return res.status(400).send({error: "Bad Request: Invalid input request"})
+    }
+    try {
+        // get all names
+        const names = loadNames().names;
+        // if names are not loaded properly, return 404 not found
+        if (!names) {
+            return res.status(404).json({error: "No names loaded from the json file.."})
+        }
+
+        const result = searchName(names, input)
+        return res.status(200).json({result})
+    }catch(error){
+        // 500 internal server error
+        return res.status(500).json({error})
+    }
+
+})
+
+
 // FUNCTIONS
 // load names from json
 function loadNames() {
@@ -41,5 +72,54 @@ function loadNames() {
     }catch(error){
         console.log({error})
         return {}
+    }
+}
+
+// check if a character is chinese
+function isChinese(char: string): boolean{
+    return new RegExp(/\p{Script=Han}/u).test(char)
+}
+
+// Step 1: split name by western and chinese name
+function splitName(name: string): string[] {
+    let resultList: string[] = []
+    // split by spaces e.g. {"Annie Lee 安妮李"} => ["Annie", "Lee", "安妮李"]
+    const nameList = name.split(/\s+/)
+    nameList.forEach(char => {
+        if (isChinese(char)){
+            resultList = resultList.concat(char.split(""))
+        }else{
+            resultList.push(char.toLowerCase())
+        }
+    })
+    return resultList
+}
+
+// search name based on input
+function searchName(names: string[], input: string): string {
+    try {
+        // solution:
+        // 1. split the western name and chinese name by utilizing regrex expression
+        //    e.g. {"Annie Lee 安妮李"} => ["Annie", "Lee", "安", "妮", "李"]
+        // 2. loop through the names and split them by step 1
+        // 3. split the input name. e.g. "Annie Lee" => ["Annie", "Lee"] or "李安妮" => ["李", "安", "妮"]
+        // 4. compare input list to name list
+        let result = ""
+        const inputCharList = splitName(input)
+        // console.log("input list: " + inputCharList)
+        // Step 2
+        names.forEach(name => {
+            const nameList = splitName(name)
+            // console.log(nameList)
+            // Step 3
+            if (inputCharList.every(item => nameList.includes(item))){
+                result = name
+            }
+        })
+        return result
+    }catch(error){
+        console.log({error})
+        // throw error to calling function
+        throw error
     }
 }
